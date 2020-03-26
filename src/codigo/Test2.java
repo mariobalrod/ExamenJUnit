626c486887307e064d54a3f632ea19a87e3bf14c
mariobalrod <marioballestero.rdg@gmail.com>
2020-03-26 17:32:56 +0100

    Correccion de la Prueba 2

--- src/codigo/Test2.java
+++ src/codigo/Test2.java
@@ -9,40 +9,29 @@ class Test2 {
 
 	Funciones f = new Funciones();
 	
-	// Son tres posibilidades
+	// Son dos posibilidades
 		// 1. Se cumplen las dos condiciones (true)
-		// 2. Se cumple solo 1(true)
-		// 3. No se cumple ninguna(false)
+		// 2. No se cumple ninguna(false)
 	
 	@DisplayName("Prueba de Blanca de Cobertura de condiciones 1")
 	@Test
 	/*
 	 * En este primero estudiamos el comportamiento de la 
-	 * funcion si cumplea ambas condiciones numero>=100 y
+	 * funcion si cumple ambas condiciones numero>=100 y
 	 * numero<=200
 	 */
 	void test1() {
 		assertTrue(f.numeroIntervalo(150));
 	}
 	
-	@DisplayName("Prueba de Blanca de Cobertura de condiciones 2")
-	@Test
-	/*
-	 * En este estudiamos el comportamiento de la 
-	 * funcion si cumplea solo una condicion por ejemplo numero>=100,
-	 * deberia de ser true.
-	 */
-	void test2() {
-		assertTrue(f.numeroIntervalo(102));
-	}
 	
-	@DisplayName("Prueba de Blanca de Cobertura de condiciones 3")
+	@DisplayName("Prueba de Blanca de Cobertura de condiciones 2")
 	@Test
 	/*
 	 * En este estudiamos el comportamiento de la 
 	 * funcion si no se cumple ninguna
 	 */
-	void test3() {
+	void test2() {
 		assertEquals(false, f.numeroIntervalo(80));
 	}
 	
