8199b8b1cfe30724cf82cb471a60a29abccfa7d8
mariobalrod <marioballestero.rdg@gmail.com>
2020-03-26 17:31:17 +0100

    Prueba 5 realizada

--- src/codigo/Test5.java
+++ src/codigo/Test5.java
@@ -2,15 +2,24 @@ package codigo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class Test5 {
 	
 	Funciones f = new Funciones();
 	
+	@DisplayName("Prueba de Caja Negra con valor límite inferor en T=300")
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	/*
+	 * Para probar el funcionamiento de nuestra funcion
+	 * con el valor limite inferior le paso el siguiente array 
+	 * dimensional tiempo, que sumaran 300.
+	 */
+	void test1() {
+		int[][] tiempo= {{100, 100, 100},{100, 100, 100},{100, 100, 100}};
+		String[] alumnos = {"javi", "javi", "javi"};
+		assertEquals(false, f.trabajosTiempo(alumnos, tiempo));
 	}
 
 }
