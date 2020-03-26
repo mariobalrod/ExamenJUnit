6f5807a87e46e014cf67c3544032efc9476333ef
mariobalrod <marioballestero.rdg@gmail.com>
2020-03-26 17:11:22 +0100

    Prueba 3 realizada

--- src/codigo/Test3.java
+++ src/codigo/Test3.java
@@ -2,15 +2,23 @@ package codigo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class Test3 {
 	
 	Funciones f = new Funciones();
 	
+	@DisplayName("Prueba de Caja Blanca Cobertura de Sentencias")
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	/*
+	 * Para cubrir todas las sentencias hay que pasarle
+	 * una cadena que solo sean numeros ya que siempre que
+	 * estudie el condicional entrará dentro. Por ejemplo,
+	 * aqui le paso "12345"
+	 */
+	void test1() {
+		assertEquals(15, f.sumaNumerosCadena("12345"));
 	}
 
 }
