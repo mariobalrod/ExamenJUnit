13b6752c2638f469439dffcda0bcf8a7a398dd24
mariobalrod <marioballestero.rdg@gmail.com>
2020-03-26 17:24:15 +0100

    Prueba 4 realizada

--- src/codigo/Test4.java
+++ src/codigo/Test4.java
@@ -2,15 +2,46 @@ package codigo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class Test4 {
 	
 	Funciones f = new Funciones();
 
+	@DisplayName("Prueba de Caja Blanca de Cobertura de Caminos 1")
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	/*
+	 * En primer lugar paso un array con longitud 0, 
+	 * para que no entre en el bucle for.
+	 * Esto dara como resultado el mismo array, es decir, 
+	 * no se ve modificado
+	 */
+	void test1() {
+		double[] array = new double[0];
+		assertArrayEquals(array, f.modArrayDoubles(array));
+	}
+	
+	@DisplayName("Prueba de Caja Blanca de Cobertura de Caminos 2")
+	@Test
+	/*
+	 * En esta pasare un array de longitud 1 para que solo entre
+	 * en la condicion de par.
+	 */
+	void test2() {
+		double[] array = {1};
+		assertArrayEquals(array, f.modArrayDoubles(array));
+	}
+	
+	@DisplayName("Prueba de Caja Blanca de Cobertura de Caminos 3")
+	@Test
+	/*
+	 * En esta pasare un array de longitud 2 para que entre
+	 * tambien en la condicion de impar
+	 */
+	void test3() {
+		double[] array = {1, 1};
+		assertArrayEquals(array, f.modArrayDoubles(array));
 	}
 
 }
