b6fc45bb6e3db6d1eb0d56d9ca31eb18219d6fbc
mariobalrod <marioballestero.rdg@gmail.com>
2020-03-26 11:17:11 +0100

    Fichero de prueba 1 preparado

---
+++ src/codigo/Test1.java
@@ -0,0 +1,16 @@
+package codigo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Test1 {
+	
+	Funciones f = new Funciones();
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
